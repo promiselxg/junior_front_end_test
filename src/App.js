eb0b015779e4126c489f724af5233d4d0368e7ec
Promiselxg <okeydeede@gmail.com>
2021-03-10 20:49:35 +0100

    updated the UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import { Container } from "@material-ui/core";
 import NavBar from "./components/NavBar";
 import AppContent from "./components/AppContent";
 
@@ -7,6 +6,7 @@ function App() {
   return (
     <div className='app'>
       <NavBar />
+      <AppContent />
     </div>
   );
 }
--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,10 +1,72 @@
 import React from "react";
+import { Container } from "@material-ui/core";
+import "../styles/dashboard.css";
+import Badge from "@material-ui/core/Badge";
 
 const AppContent = () => {
   return (
     <>
-      <div className='appContent'>
-        <h1>App content</h1>
+      <div className='dashboard'>
+        <Container>
+          <div className='dashboard__container'>
+            <div className='dashboard__top'>
+              <div className='dashboard__topLeft'>
+                <h1>Hey,Terry</h1>
+                <h2>
+                  Start your <span>30 days</span> Trial
+                </h2>
+                <p>Select your preferred package below</p>
+              </div>
+              <div className='dashboard__topRight'>
+                <h1>NGN</h1>
+                <h1 className='currency'>1,999,999</h1>
+                <h2>
+                  Total <span>(Excludes Vat)</span>
+                </h2>
+              </div>
+            </div>
+            <div className='dashboard__options'>
+              <div className='dashboard__option default'>
+                <div className='dashboard__optionBox'>
+                  <div className='option_one'>
+                    <h1>Starts From</h1>
+                    <h2>NGN</h2>
+                    <h1>5,999,999.99</h1>
+                    <p>Per annum</p>
+                  </div>
+                  <div className='option_two'>
+                    <h1>Premium</h1>
+                    <span className='bottom'>
+                      <h2>100 Users</h2>
+                      <h2>.</h2>
+                      <h2>100 Users</h2>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className='highlight'>
+                <div className='dashboard__optionBox'>
+                  <div className='option_one'>
+                    <h1>Basic</h1>
+                    <p>
+                      <span>30 days trial</span> on our premium app features
+                    </p>
+                    <span className='second'>
+                      <p>Includes -------------</p>
+                      <p>- 25 users</p>
+                      <p>- all modules and features</p>
+                      <p>- unlimited commitees</p>
+                      <p>- unlimited role creation</p>
+                    </span>
+                  </div>
+                </div>
+                <span className='badge'>
+                  <i className='fa fa-check' />
+                </span>
+              </div>
+            </div>
+          </div>
+        </Container>
       </div>
       ;
     </>
